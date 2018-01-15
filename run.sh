e3b3765c781caa0ee8afbd96a4a1b9af89d33457
Will Zegers <will.zegers@gmail.com>
2018-01-14 17:10:42 -0800

    Adding TF ObjDet library and functionality

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+./protoc/bin/protoc object_detection/protos/*.proto --python_out=.
 export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+python object_detection/builders/model_builder_test.py
---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/bin/nash
+
+python object_detection/train.py --logtostderr --train_dir=data/ --pipeline_config_path=data/pipeline.config
